233d0377f1bb6b18aca7120eca9c6a4a813b9216
cjiali <chjiali@outlook.com>
2020-11-01 11:02:51 +0800

    chore: use API Extractor

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,26 @@
+/**
+ * 加法运算
+ * @param a - 加数
+ * @param b - 被加数
+ * @returns the result of a plus b
+ * @example
+ * ```ts
+ * minus(1, 2) => 2
+ * ```
+ *
+ * @beta
+ */
 export declare function add(a: number, b: number): number;
+/**
+ * 减法运算
+ * @param a - 减数
+ * @param b - 被减数
+ * @returns the result of a minus b
+ * @example
+ * ```ts
+ * minus(3, 2) => 1
+ * ```
+ *
+ * @beta
+ */
 export declare function minus(a: number, b: number): number;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-export function add(a: number, b: number){
+/**
+ * 加法运算
+ * @param a - 加数
+ * @param b - 被加数
+ * @returns the result of a plus b
+ * @example
+ * ```ts
+ * minus(1, 2) => 2
+ * ```
+ * 
+ * @beta
+ */
+export function add(a: number, b: number) {
     return a + b;
 }
 
-export function minus(a: number, b: number){
+/**
+ * 减法运算
+ * @param a - 减数
+ * @param b - 被减数
+ * @returns the result of a minus b
+ * @example
+ * ```ts
+ * minus(3, 2) => 1
+ * ```
+ * 
+ * @beta
+ */
+export function minus(a: number, b: number) {
     return a - b;
 }
